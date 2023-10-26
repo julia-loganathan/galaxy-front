22ed094b42052faba282fc48c9625cacdc59438b
ocherif <cherifoussama1996@gmail.com>
2023-10-26 17:19:38 +0200

    react dom version correction

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
@@ -10,7 +10,9 @@ import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <BrowserRouter>
@@ -18,8 +20,6 @@ ReactDOM.render(
       </BrowserRouter>
     </AuthContextProvider>
   </React.StrictMode>,
-
-  document.getElementById('root')
 
 );
 
