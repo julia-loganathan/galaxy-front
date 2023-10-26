b1530dd9051f187ec220c8c1d2bd9eb9117e97ea
faresmez <133017548+faresmez@users.noreply.github.com>
2023-10-27 01:06:27 +0200

    PopUp added

--- src/core/pages/HomePage.js
+++ src/core/pages/HomePage.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import ReservationPopup from './reservation/ReservationPopup';
 
 function HomePage() {
     const [destinations, setDestinations] = useState([]);
     const [error, setError] = useState(null);
+    const [reservations, setReservations] = useState([]);
+    const [user, setUser] = useState(null);
 
+    const [showModals, setShowModals] = useState({});
+    const [selectedDestination, setSelectedDestination] = useState(null);
+    const [nombrePlaces, setNombrePlaces] = useState(1);
+    const [dateDebutVoyage, setDateDebutVoyage] = useState('');
 
-    const handleParticipateClick = (destination) => {
-
-        console.log(`Participer à un voyage pour ${destination.name}`);
+    const openModal = (destination) => {
+        setSelectedDestination(destination);
+        setShowModals({ ...showModals, [destination._id]: true });
     };
+
+    const closeModal = (destination) => {
+        setSelectedDestination(null);
+        setShowModals({ ...showModals, [destination._id]: false });
+    };
+
+    const handleReservation = () => {
+        try {
+        } catch (error) {
+          console.error('Error while marking attendance:', error);
+        }
+      };
 
     useEffect(() => {
         fetch('http://localhost:4000/api/destinations/destination', { method: 'GET', mode: 'cors' })
@@ -28,33 +48,45 @@ function HomePage() {
 
     return (
         <div>
-            <h1>Catalogue des destinations</h1>
+            <h1 className="text-center">Catalogue des destinations</h1>
             {error ? (
                 <p>Erreur : {error}</p>
             ) : (
-                <div className="destination-list">
+                <Row xs={1} md={2} className="justify-content-center">
                     {destinations.map((destination) => (
-                        <div key={destination._id} className="destination-item">
-                            <h2 className="destination-name">{destination.name}</h2>
-                            {destination.imageUrl && (
-                                <img
-                                    src={destination.imageUrl}
-                                    alt={destination.name}
-                                    className="destination-image"
+                        <Col key={destination._id}>
+                            <Card className="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+                                <Card.Body className="text-center">
+                                    <Card.Title>{destination.name}</Card.Title>
+                                    <Card.Text>{destination.description}</Card.Text>
+                                </Card.Body>
+                                <div className="d-flex justify-content-center align-items-center" style={{ maxWidth: '500px', maxHeight: '500px' }}>
+                                    <Card.Img
+                                        variant="top"
+                                        style={{ maxWidth: '500px', maxHeight: '500px' }}
+                                        src={destination.imageUrl}
+                                    />
+                                </div>
+                                <Button variant="outline-primary" onClick={() => openModal(destination)}>
+                                    Participer à un voyage
+                                </Button>
+                                <ReservationPopup
+                                    show={showModals[destination._id] || false}
+                                    onHide={() => closeModal(destination)}
+                                    destination={destination}
+                                    onReservation={handleReservation}
+                                    nombrePlaces={nombrePlaces}
+                                    dateDebutVoyage={dateDebutVoyage}
+                                    onClose={() => setShowModals(false)}
                                 />
-                            )}
-                            <p className="destination-description">{destination.description}</p>
-                            <p className="destination-durée">{`Durée : ${destination.durée}`}</p>
-
-                            <button onClick={() => handleParticipateClick(destination)}>
-                                Participer à un voyage
-                            </button>
-                        </div>
+                            </Card>
+                        </Col>
                     ))}
-                </div>
+                </Row>
             )}
         </div>
     );
+    
 }
 
 export default HomePage;
---
+++ src/core/pages/reservation/ReservationPopup.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+
+function ReservationPopup({ show, onHide, destination, onReservation, onClose }) {
+    const [nombrePlaces, setNombrePlaces] = useState(1);
+    const [dateDebutVoyage, setDateDebutVoyage] = useState('');
+    const [showValidationMessage, setShowValidationMessage] = useState(false);
+
+
+    const handleReservation = async () => {
+      try {
+        const reservationData = {
+          voyage: destination._id,
+          nombrePlaces: nombrePlaces,
+          dateDebutVoyage: dateDebutVoyage,
+        };
+    
+        const response = await fetch('http://localhost:4000/reservations/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(reservationData),
+        });
+
+        if (response.status === 201) {
+          onReservation(destination, nombrePlaces, dateDebutVoyage);
+          setShowValidationMessage(true);
+        } else {
+          console.error('Échec de la réservation: ' + response.status);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la réservation:', error);
+      }
+    };
+    
+
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title>Réservation de Voyage pour {destination.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Nombre de Places</Form.Label>
+                        <Form.Control
+                            type="number"
+                            value={nombrePlaces}
+                            onChange={(e) => setNombrePlaces(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Date du Voyage</Form.Label>
+                        <Form.Control
+                            type="date"
+                            value={dateDebutVoyage}
+                            onChange={(e) => setDateDebutVoyage(e.target.value)}
+                        />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    Annuler
+                </Button>
+                {showValidationMessage ? (
+                    <div className="validation-message">Votre réservation a été validée !</div>
+                ) : (
+                    <Button variant="primary" onClick={handleReservation}>
+                        Valider
+                    </Button>
+                )}
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default ReservationPopup;
