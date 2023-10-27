63427f9b7275f5573ead061f4d6909eb9868a846
faresmez <133017548+faresmez@users.noreply.github.com>
2023-10-27 09:45:54 +0200

    modification index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import App from './App';
-
-import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './core/context/AuthContext';
-
-
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
